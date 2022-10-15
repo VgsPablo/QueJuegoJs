af59c5f1623fc8d26a4e246a895ce2b98209b260
Pablo Vargas <jjpvargas97@gmail.com>
2022-10-15 17:34:56 -0300

    agregado carrito de compras

--- js/queJuego.js
+++ js/queJuego.js
@@ -1,7 +1,8 @@
-let nombreCompleto = "Pablo Vargas"
+let name = "Pablo Vargas"
 
 function saludar(usuario) {
     console.log("Bienvenid@ 🙋🏻‍♂️", usuario)
+    alert("Bienvenid@ 🙋🏻‍♂️")
 }
 
 function login() {
@@ -11,31 +12,73 @@ function login() {
             saludar(usuario)
         } else {
             console.warn("No se reconoce el usuario.")
+            alert("No se reconoce el usuario.")
         }
 }
 
-function verJuegos() {
-    let juegos = "Juegos: \n" +
-                    "1) Fifa 23 \n" +
-                    "2) The Elder Scrolls V: Skyrim \n" +
-                    "3) Cyberpunk 2077"
+const juegos = [
+    { nombre: "Fifa 23", precio: 23000 },
+    { nombre: "The Elder Scrolls V: Skyrim", precio: 12000 },
+    { nombre: "Cyberpunk 2077", precio: 3000 },
+]
 
-    let respuesta = prompt("Ingresa numero de juego para ver el precio: \n" + juegos)
-    switch (parseInt(respuesta)) {
-        case 1:
-            console.log("Fifa 23. Precio: $23.000")
-            break;
-        case 2:
-            console.log("The Elder Scrolls V: Skyrim. Precio: $12.000")
-            break;
-        case 3:
-            console.log("Cyberpunk 2077. Precio: $3.000")
-            break;
-        default:
-            console.error("No se reconoce el numero ingresado.")
-    }
-    let continuar = confirm("¿Seguir consultando?")
-    if (continuar) {
-        verJuegos()
-    }
+let carrito = []
+
+let bienvenida = prompt("¡Hola! ¿Queres ver nuestra lista de juegos disponibles? Responde SI o NO")
+
+while(bienvenida != "SI" && bienvenida != "NO"){
+    alert("Respuesta invalida. Por favor responder SI o NO")
+    bienvenida = prompt("¡Hola! ¿Queres ver nuestra lista de juegos disponibles? Responde SI o NO")
 }
+
+if(bienvenida == "SI"){
+    let listaJuegos = juegos.map(
+        (juego) => juego.nombre + " " + "$" + juego.precio
+    );
+    alert(listaJuegos.join(" \n "))
+}
+
+else if(bienvenida == "NO"){
+    alert("¡Gracias por visitarnos!")
+}
+
+while(bienvenida != "NO"){
+    let juego = prompt("Agrega un juego a tu carrito")
+    let precio = 0
+
+    if(juego == "Fifa 23" || juego == "The Elder Scrolls V: Skyrim" || juego == "Cyberpunk 2077" ){
+        switch (juego){
+            case "Fifa 23":
+                precio = 23000;
+                break;
+            case "The Elder Scrolls V: Skyrim":
+                precio = 12000;
+                break;    
+            case "Cyberpunk 2077":
+                precio = 3000;
+                break;
+            default:
+                break;    
+        }
+
+        carrito.push(juego, precio)
+        console.log(carrito)
+    }
+
+    else {
+        alert("No encontramos el juego.")
+    }
+
+    bienvenida = prompt("¿Queres seguir comprando?")
+    while(bienvenida === "NO"){
+        alert("¡Gracias por tu compra!")
+        carrito.forEach((carritoFinal) =>{
+            console.log(`juego: ${carritoFinal.juego}, total ${carritoFinal.precio}`)
+        })
+        break;
+    }
+
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+console.log(`El total a pagar es: ${total}`)
